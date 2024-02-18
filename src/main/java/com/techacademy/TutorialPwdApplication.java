22ff93cedcdd702b333e3996d11ba077048a59b4
shingostar <shingostar@shore.ocn.ne.jp>
2024-02-18 13:22:51 +0900

    PWD確認のためコミットします。

---
+++ src/main/java/com/techacademy/TutorialPwdApplication.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialPwdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialPwdApplication.class, args);
+	}
+
+}
